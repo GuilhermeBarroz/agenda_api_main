4bb0410940a1adb41faf3c85db6c22c7d5fff50f
Guilherme Barros <106363792+GuilhermeBarroz@users.noreply.github.com>
2023-06-13 07:04:02 -0300

    udpates

--- agenda_api/src/main/java/com/restapi/agenda/Entidade/Cadastro.java
+++ agenda_api/src/main/java/com/restapi/agenda/Entidade/Cadastro.java
@@ -7,21 +7,21 @@ import jakarta.persistence.*;
 public class Cadastro {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long ID_Matricula;
+    private Long id_matricula;
     @Column(nullable = false, length = 250)
     private String nome_usuario;
     @Column(nullable = false, length = 250, unique = true)
-    private String email;
+    private String email_usuario;
     @Column(nullable = false, length = 50)
-    private String senha;
+    private String senha_usuario;
 
 
     public Long getIdMatricula() {
-        return ID_Matricula;
+        return id_matricula;
     }
 
     public void setIdMatricula(Long idMatricula) {
-        ID_Matricula = idMatricula;
+        id_matricula = idMatricula;
     }
 
     public String getNome() {
@@ -33,18 +33,18 @@ public class Cadastro {
     }
 
     public String getEmail() {
-        return email;
+        return email_usuario;
     }
 
     public void setEmail(String email) {
-        this.email = email;
+        this.email_usuario = email;
     }
 
     public String getSenha() {
-        return senha;
+        return senha_usuario;
     }
 
     public void setSenha(String senha) {
-        this.senha = senha;
+        this.senha_usuario = senha;
     }
 }
